59119bdcc4f98495942ff37e59712a981143a83d
Victor Groux <vgroux@pop-os.localdomain>
2022-10-19 22:53:58 +0200

    modif putnbr_base

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,27 +6,36 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 19:22:08 by vgroux            #+#    #+#             */
-/*   Updated: 2022/10/18 19:24:59 by vgroux           ###   ########.fr       */
+/*   Updated: 2022/10/19 22:51:12 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putnbr_base(int n, int base, int fd)
+void	ft_putnbr_base_fd(int n, char *base, int fd)
 {
 	long	num;
+	int		baselen;
 
 	num = n;
+	baselen = ft_strlen(base);
 	if (num < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putnbr_fd(num * -1, fd);
+		ft_putnbr_base_fd(num * -1, base, fd);
 	}
-	else if (num > base)
+	else if (num > baselen)
 	{
-		ft_putnbr_fd(num / base, fd);
-		ft_putnbr_fd(num % base, fd);
+		ft_putnbr_base_fd(num / baselen, base, fd);
+		ft_putnbr_base_fd(num % baselen, base, fd);
 	}
 	else
-		ft_putchar_fd(num + '0', fd);
+		ft_putchar_fd(base[num], fd);
+}
+
+int	main(void)
+{
+	int n = 15;
+
+	ft_putnbr_base_fd(n, "0123456789abcdef", 1);
 }
